944d47242ade2c66c6aa09e726480f596488b167
gabriel <gabriel.amoreira@outlook.com>
2023-06-27 12:18:23 -0300

    First Commit

---
+++ app/src/main/java/br/com/alura/aluvery/ui/activies/ProductFormActivity.kt
@@ -0,0 +1,110 @@
+package br.com.alura.aluvery.ui.activies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.alura.aluvery.ui.theme.AluveryTheme
+
+class ProductFormActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AluveryTheme {
+                Surface {
+                    ProductFormScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductFormScreen() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "Criando o produto",
+            Modifier.fillMaxWidth(),
+            fontSize = 28.sp,
+        )
+        var url by remember {
+            mutableStateOf("")
+        }
+        TextField(value = url, onValueChange = {
+            url = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Url de imagem")
+        })
+
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {
+            name = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Nome")
+        })
+
+        var price by remember {
+            mutableStateOf("")
+        }
+        TextField(value = price, onValueChange = {
+            price = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Preço")
+        })
+
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(value = description, onValueChange = {
+            description = it
+        },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp), label = {
+            Text(text = "Descrição")
+        })
+
+        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+            Text(text = "Salvar")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
+    AluveryTheme {
+        Surface {
+            ProductFormScreen()
+        }
+    }
+
+}
+        
+
